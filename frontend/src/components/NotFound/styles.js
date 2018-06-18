385ace12afed35604385039eade7d0dc9966aadc
Michael Staub <mikestaub22@gmail.com>
2018-06-17 22:00:24 -0700

    add missing file

---
+++ frontend/src/components/NotFound/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  height: 250px;
+
+  > h1 {
+    font-size: 30px;
+    padding: 20px;
+  }
+`;
+
+export { Container };
